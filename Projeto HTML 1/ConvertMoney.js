29b391dc46e4c070b384f7d0113fc8c638265afe
petersondasilvagomes <petersondasilvagomes@hotmail.com>
2023-02-14 10:32:10 -0300

    Convert Money

---
+++ Projeto HTML 1/ConvertMoney.js
@@ -0,0 +1,24 @@
+const button = document.getElementById("convert-button")
+
+const dolar = 5.2
+
+const convertValues = () => {
+    const inputReais = document.getElementById("input-real").value
+    const realValueText = document.getElementById("real-value-text")
+    const currencyValueText = document.getElementById("currency-value-text")
+
+    realValueText.innerHTML = inputReais
+    currencyValueText.innerHTML = inputReais / dolar
+
+    realValueText.innerHTML=new Intl.NumberFormat('pt-BR',
+    {style:"currency",
+     currency:"BRL",
+    }).format(inputReais);
+
+    currencyValueText.innerHTML=new Intl.NumberFormat('en-US',
+    {style:"currency",
+     currency:"USD",
+    }).format(inputReais/dolar);
+};
+
+button.addEventListener("click", convertValues)
